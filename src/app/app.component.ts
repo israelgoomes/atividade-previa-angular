4fd9940243bf3500270b7713668d85f6ddcab105
israelgoomes <rael_goomes@hotmail.com>
2020-10-01 17:43:51 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  gasolina: any;
+  alcool: any;
+
   title = 'atividade-previa-angular';
+
+  calc() {
+    if (!this.alcool || !this.gasolina) {
+      alert('Digite um valor para calcular');
+      return;
+    }
+
+    let division = this.alcool / this.gasolina;
+
+    if (division < 0.7) {
+      alert('Abasteça com Álcool!')
+    } else {
+      alert('Abasteça com gasolina!');
+    }
+
+
+  }
 }
